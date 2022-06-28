b6ff8c95adfc742862b5079ac9d79a06cb8854cf
Gastón Cuello <gastonec16@gmail.com>
2022-06-28 16:35:58 -0300

    intentando guardar perfil al registrarse

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -5,24 +5,30 @@ from .models import Perfil
 
 class FormRegistroUsuario(UserCreationForm):
     email = forms.EmailField()
-    first_name = forms.CharField(label='Nombre')
-    last_name = forms.CharField(label='Apellido')
-    legajo = forms.IntegerField()
-    telefono = forms.IntegerField(label='Teléfono', required=False)
     is_staff = forms.BooleanField(label='Es del equipo técnico', required=False)
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'legajo', 'telefono', 'password1', 'password2', 'is_staff']
+        fields = ['username', 'email', 'password1', 'password2', 'is_staff']
+
+class FormRegistroPerfil(forms.ModelForm):
+    nombre = forms.CharField()
+    apellido = forms.CharField()
+    legajo = forms.IntegerField()
+    telefono = forms.IntegerField(label='Teléfono', required=False)
+
+    class Meta:
+        model = Perfil
+        fields = ['nombre', 'apellido', 'legajo', 'telefono']
 
 class FormActualizarUsuario(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = ['username', 'email']
 
 class FormActualizarPerfil(forms.ModelForm):
     telefono = forms.IntegerField(label='Teléfono', required=False)
 
     class Meta:
         model = Perfil
-        fields = ['imagen', 'legajo', 'telefono']
+        fields = ['nombre', 'apellido', 'imagen', 'legajo', 'telefono']
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
 
 class Perfil(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=150, default='Nombre')
+    apellido = models.CharField(max_length=150, default='Apellido')
     imagen = models.ImageField(default='perfil-default.jpg', upload_to='fotos-perfil')
     legajo = models.IntegerField(blank=True, null=True)
     telefono = models.IntegerField(blank=True, null=True)
